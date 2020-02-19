79dc679cb541d84682564f8b8b5399ba2ce9cbb7
Denisa Dupu <dupudenisa@gmail.com>
2020-02-18 23:17:44 -0600

    adding variables for future westher api

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//Button on click function
+
+var searchHistory = document.querySelector("#search-history");
+
+
 
 $(".search-btn").click(function (e) {
 
@@ -6,7 +9,8 @@ $(".search-btn").click(function (e) {
     var cityname = $("#cityName").val();
 
     var date = new Date();
-    var today = date.getDate();
+
+    var today = date.getUTCDate();
 
     var limit = 5;
 
@@ -17,46 +21,77 @@ $(".search-btn").click(function (e) {
         url: urlSearch,
         method: "GET"
 
-    }).then(function(response){
+    }).then(function (response) {
 
         console.log(response);
 
         var city = response.city.name;
-        var temp = response.main.temp;
-        var condition = response.weather;
+        var tempToday = response.list[0].main.temp;
+        var humidityToday = response.list[0].main.humidity;
+        var windToday = response.list[0].wind.speed;
+        //var temp = response.main.temp;
+
+        var temp1 = response.list[0].main.temp;
+        var humidity1 = response.list[0].main.humidity;
+        var wind1 = response.list[0].wind.speed;
+
+        var temp2 = response.list[6].main.temp;
+        var humidity2 = response.list[6].main.humidity;
+        var wind2 = response.list[6].wind.speed;
+
+        var temp3 = response.list[13].main.temp;
+        var humidity3 = response.list[13].main.humidity;
+        var wind3 = response.list[13].wind.speed;
 
 
+        var temp4 = response.list[22].main.temp;
+        var humidity4 = response.list[22].main.humidity;
+        var wind4 = response.list[22].wind.speed;
 
 
+        var temp5 = response.list[30].main.temp;
+        var humidity5 = response.list[30].main.humidity;
+        var wind5 = response.list[30].wind.speed;
 
 
+        $("#city-header").text(city + " " + today);
 
-        $("#city-header").text(city + " " + today + " " + condition);
+        //$("#condition-img").attr('src', conditionT);
 
-        $("#temp").text("Temperature: " + temp);
+        $("#temp").text("Temperature: " + tempToday);
+        $("#humidity").text("Humidity: " + humidityToday);
+        $("#wind-speed").text("Wind Speed: " + windToday);
 
+        $("#temp-1").text("Temperature: " + tempToday);
+        $("#humidity-1").text("Humidity: " + humidityToday);
+        $("#wind-speed-1").text("Wind Speed: " + windToday);
+
+        $("#temp-2").text("Temperature: " + tempToday);
+        $("#humidity-2").text("Humidity: " + humidityToday);
+        $("#wind-speed-2").text("Wind Speed: " + windToday);
+
+        $("#temp-3").text("Temperature: " + tempToday);
+        $("#humidity-3").text("Humidity: " + humidityToday);
+        $("#wind-speed-3").text("Wind Speed: " + windToday);
+
+        $("#temp-4").text("Temperature: " + tempToday);
+        $("#humidity-4").text("Humidity: " + humidityToday);
+        $("#wind-speed-4").text("Wind Speed: " + windToday);
+
+        $("#temp-5").text("Temperature: " + tempToday);
+        $("#humidity-5").text("Humidity: " + humidityToday);
+        $("#wind-speed-5").text("Wind Speed: " + windToday);
+
+    }).then(function createHistory() {
+        var searches = [];
+
+        var li = document.createElement("li");
+        li.textContent = cityname;
+        searchHistory.appendChild(li);
+
+        searches.push(cityname);
 
 
     })
 
-    /*
-
-   $.getJSON(urlSearch, function(data) {
-
-        console.log(data);
-
-        city_name = data["name"];
-        country_name = data["sys"]["country"];
-        weather_description = data["weather"][0]["description"];
-        temp = data["main"]["temp"];
-        pressure = data["main"]["pressure"];
-        wind_speed = data["wind"]["speed"];
-
-       // $("#cityname").html(city_name + " &#40;" + country_name + "&#41; " + "has " + weather_description);
-        $("#temp").text(temp);
-       // $(".pressure").html(pressure + " mBar");
-        //$(".wind-spd").html(wind_speed + " m/s");
-
-      })
-    */
 })
