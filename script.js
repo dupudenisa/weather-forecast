7dac8742a73beba7e2191e8e35289468663c3fbc
Denisa Dupu <dupudenisa@gmail.com>
2020-02-19 23:59:31 -0600

    added weather icons

--- script.js
+++ script.js
@@ -26,9 +26,13 @@ $(".search-btn").click(function (e) {
         var tempToday = dataCurr.main.temp;
         var humidityToday = dataCurr.main.humidity;
         var windToday = dataCurr.wind.speed;
+        var icon = dataCurr.weather[0].icon;
+        var iconLink =  "http://openweathermap.org/img/w/" + icon + ".png";
 
 
         $("#city-header").text(city + " " + today);
+        console.log(icon);
+        $("#condition").attr('src', iconLink);
         $("#temp").text("Temperature: " + tempToday);
         $("#humidity").text("Humidity: " + humidityToday);
         $("#wind-speed").text("Wind Speed: " + windToday);
@@ -79,46 +83,60 @@ $(".search-btn").click(function (e) {
         console.log(response);
 
         var temp1 = response.list[0].main.temp;
+        var icon1 = response.list[0].weather[0].icon;
         var humidity1 = response.list[0].main.humidity;
         var wind1 = response.list[0].wind.speed;
+        var iconLink1 =  "http://openweathermap.org/img/w/" + icon1 + ".png";
 
         var temp2 = response.list[6].main.temp;
+        var icon2 = response.list[6].weather[0].icon;
         var humidity2 = response.list[6].main.humidity;
         var wind2 = response.list[6].wind.speed;
+        var iconLink2 =  "http://openweathermap.org/img/w/" + icon2 + ".png";
 
         var temp3 = response.list[13].main.temp;
+        var icon3 = response.list[13].weather[0].icon;
         var humidity3 = response.list[13].main.humidity;
         var wind3 = response.list[13].wind.speed;
+        var iconLink3 =  "http://openweathermap.org/img/w/" + icon3 + ".png";
 
 
         var temp4 = response.list[22].main.temp;
+        var icon4 = response.list[22].weather[0].icon;
         var humidity4 = response.list[22].main.humidity;
         var wind4 = response.list[22].wind.speed;
-
+        var iconLink4 =  "http://openweathermap.org/img/w/" + icon4 + ".png";
 
         var temp5 = response.list[30].main.temp;
+        var icon5 = response.list[30].weather[0].icon;
         var humidity5 = response.list[30].main.humidity;
         var wind5 = response.list[30].wind.speed;
+        var iconLink5 =  "http://openweathermap.org/img/w/" + icon5 + ".png";
 
         // getting the html variables and setting them to the values i have taken from future weather api
 
         $("#temp-1").text("Temperature: " + temp1);
+        $("#condition-1").attr('src', iconLink1);
         $("#humidity-1").text("Humidity: " + humidity1);
         $("#wind-speed-1").text("Wind Speed: " + wind1);
 
         $("#temp-2").text("Temperature: " + temp2);
+        $("#condition-2").attr('src', iconLink2);
         $("#humidity-2").text("Humidity: " + humidity2);
         $("#wind-speed-2").text("Wind Speed: " + wind2);
 
         $("#temp-3").text("Temperature: " + temp3);
+        $("#condition-3").attr('src', iconLink3);
         $("#humidity-3").text("Humidity: " + humidity3);
         $("#wind-speed-3").text("Wind Speed: " + wind3);
 
         $("#temp-4").text("Temperature: " + temp4);
+        $("#condition-4").attr('src', iconLink4);
         $("#humidity-4").text("Humidity: " + humidity4);
         $("#wind-speed-4").text("Wind Speed: " + wind4);
 
         $("#temp-5").text("Temperature: " + temp5);
+        $("#condition-5").attr('src', iconLink5);
         $("#humidity-5").text("Humidity: " + humidity5);
         $("#wind-speed-5").text("Wind Speed: " + wind5);
 
